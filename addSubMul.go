4f77ce22dd3b962c65ae6a550505ff99d7e609e9
omar115 <omarhasan115@gmail.com>
2020-12-28 14:28:08 +0600

    variables

---
+++ addSubMul.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	var number uint = 260 //var declare
+	number = number + 5   //add
+	fmt.Println("hello ", number)
+	number = number - 3 //sub
+	fmt.Println("hello ", number)
+	number = number / 2 //div
+	fmt.Println("hello ", number)
+	number = number % 2 //reminder
+	fmt.Println("hello ", number)
+	number = number * 20 //multiply
+	fmt.Println("hello ", number)
+}
---
+++ variables.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	var number uint = 260 //var declare
+	fmt.Println("hello \n", number)
+	var num = 5
+	fmt.Printf("%T", num) //t means type
+	var num2 = 5.55
+	fmt.Printf("%T", num2)
+	//fast way to declare var name
+	num3 := "Omar"
+	fmt.Printf("%T", num3)
+}
